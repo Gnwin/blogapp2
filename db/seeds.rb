891495bf6131df30a11bb87d620aaab647f9113c
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-16 23:32:50 +0100

    seed db to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
 second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
-
 first_post = Post.create(user: first_user, title: 'Hello', body: 'This is my first post')
+first_post.increment_user_posts_count
 first_comment = Comment.create(post: first_post, user: second_user, body: 'Hi Tom!' )
+first_comment.increment_posts_comments_counter
