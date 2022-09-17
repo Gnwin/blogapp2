70b1072ac1ce6eec8b6670e9845a7d96a918744f
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-17 09:27:27 +0100

    add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,12 @@ class Post < ApplicationRecord
 
   after_save :increment_user_posts_count
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250, too_long: 'The maximum characters allowed for a Title is 250 characters' }
+  validates :text, presence: true, length: { maximum: 4000 }
+  validates :comments_counter, comparison: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, comparison: { only_integer: true, greater_than_or_equal_to: 0 }
+
   private
 
   def increment_user_posts_count
