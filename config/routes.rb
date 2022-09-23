ea091889211ea852af053ce23c4683f7a7e4fc27
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-23 13:30:59 +0100

    add forms

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,15 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'users#index'
 
-  get '/users', to: 'users#index'
-  get '/users/:id', to: 'users#show'
-  get '/users/:user_id/posts' => 'posts#index', as: :user_posts
-  get '/users/:user_id/posts/:id' => 'posts#show', as: :user_post
+  # get '/users', to: 'users#index'
+  # get '/users/:id', to: 'users#show'
+  # get '/users/:user_id/posts' => 'posts#index', as: :user_posts
+  # get '/users/:user_id/posts/:id' => 'posts#show', as: :user_post
 
-  # resources: users
+  resources :users, only: %i[index show] do
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+      resources :likes, only: [:create]
+    end
+  end
 end
