14d1859970336031e31f8e539ee4297140270d13
Godwin Nwachukwu <nwgwin@gmail.com>
2022-09-23 09:57:40 +0100

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.where(user: params[:user_id])
+    @all_posts = Post.where(user: params[:user_id])
+    @comments = Comment.where(post_id: params[:user_id])
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @user = User.find(params[:user_id])
+    @comments = Comment.where(post_id: params[:id])
+  end
 end
